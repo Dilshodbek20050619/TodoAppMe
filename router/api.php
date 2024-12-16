b4c6d9326f453e8842d155d05704845a65ac5798
Dilshodbek20050619 <erolovdilshod019@gmail.com>
2024-12-16 17:19:47 +0500

    m

--- router/api.php
+++ router/api.php
@@ -1,16 +1,16 @@
 <?php
-
-use App\Router;
-
-require 'src/Router.php';
-require 'src/Todo.php';
-
-$router=new Router();
-$todo=new \App\Todo();
-
-$router->get('/api/todos',function () use ($todo){
-    apiResponse($todo->getAllTodos(2));
-});
-$router->get('/api/todos/{id}',function ($todoId)use ($todo){
-    apiResponse($todo->getTodo($todoId));
-});
+//
+//use App\Router;
+//
+//require 'src/Router.php';
+//require 'src/Todo.php';
+//
+//$router=new Router();
+//$todo=new \App\Todo();
+//
+//$router->get('/api/todos',function () use ($todo){
+//    apiResponse($todo->getAllTodos(2));
+//});
+//$router->get('/api/todos/{id}',function ($todoId)use ($todo){
+//    apiResponse($todo->getTodo($todoId));
+//});
