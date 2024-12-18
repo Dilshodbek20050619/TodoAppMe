e1b14c9eb3cbbb378fa0b05598b071a0ddff244e
Dilshodbek20050619 <erolovdilshod019@gmail.com>
2024-12-18 16:58:43 +0500

    git :L

--- routers/telegram.php
+++ routers/telegram.php
@@ -1,17 +1,30 @@
 <?php
+
+use App\Bot;
+
+$bot = new Bot();
+
 $update=json_decode(file_get_contents('php://input'));
 $chatId=$update->message->chat->id;
 $text=$update->message->text;
 
-use GuzzleHttp\Client;
-
-$client = new Client([
-    'base_uri' => "https://api.telegram.org/bot" . $_ENV['TELEGRAM_TOKEN'] . "/",
-]);
-
-$client->post('sendMessage',[
-    'form_params'=>[
+if ($text == "/start") {
+    $bot->makeRequest('sendMessage', [
+        'form_params'=>[
+            'chat_id'=>$chatId,
+            'text'=>'hello welcome to telegram',
+        ]
+    ]);
+    exit();
+}
+if (mb_stripos($text, '/start')!==false){
+    $userId = explode('/start', $text)[1];
+    $taskList = "";
+    $bot->makeRequest('sendMessage',[
         'chat_id'=>$chatId,
-        'text'=>$text,
-    ]
-]);
+        'text'=>'Welcome to the Todo App (mb_stripos) ' . $userId
+    ]);
+    exit();
+}
+
+
